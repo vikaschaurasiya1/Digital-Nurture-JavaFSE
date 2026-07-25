32e1fbbb5b0ce0d51127bef921303ec5ac4ffec3
vikaschaurasiya1 <vikaschaurasiya012@gmail.com>
2026-07-25 12:58:54 +0530

    Rename Week-5_Angular-React to Week-5_React

---
+++ Deep-Skilling/Week-5_React/01_ReactJS/Exercise_01/Code/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1>Welcome the first session of React</h1>
+    </>
+  );
+}
+
+export default App;
